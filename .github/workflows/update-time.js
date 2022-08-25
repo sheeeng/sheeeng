2ad3c108eacc99ffcb205d917edfbb458745f0c7
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-08-25 16:55:32 +0200

    feature: update time

---
+++ .github/workflows/update-time.js
@@ -0,0 +1,28 @@
+const query = `mutation changeUserStatus ($input: ChangeUserStatusInput!) {
+  changeUserStatus (input: $input) {
+    status {
+      emoji
+      message
+    }
+  }
+}`
+
+privateClassMethod(this.clockEmoji = function (time) {
+  // Hour as a decimal number using a 12-hour clock (range 1 to 12).
+  let hour = time.strftime("%l").toI;
+
+  // Minute as a decimal number (range 00 to 59).
+  let minute = time.strftime("%M").toI;
+
+  // If minute is between 15 and 45, use the emoji clocks
+  // that point at minute 30. For example: 🕦 or 🕝.
+  let isNearHalfHour = minute > 15 && minute < 45;
+  let thirtyMinutesOrBlank = isNearHalfHour ? "30" : "";
+
+  // Return the clock emoji that most resembles the current time
+  `:clock${hour}${thirtyMinutesOrBlank}:`
+})
+
+module.exports = async ({ github, context, core }) => {
+  return time.strftime("It's %l:%M%P in :norway:.") + "•───•°•❀•°•───•" + clockEmoji;
+}
