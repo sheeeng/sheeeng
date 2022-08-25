aa0c5e9a57e5a7317b6815cc0ac0858aff0b881b
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2022-08-25 14:38:12 +0200

    chore: limited availability

--- .github/workflows/update-status.js
+++ .github/workflows/update-status.js
@@ -18,7 +18,7 @@ module.exports = async ({ github, context, core }) => {
     input: {
       emoji: `${emojis[dayIndex]}`,
       message: `It's ${dayNames[dayIndex]}! ${kaomojis[dayIndex]}`,
-      limitedAvailability: limitedAvailabilityDayNames.has(dayNames[dayIndex])
+      limitedAvailability: !limitedAvailabilityDayNames.has(dayNames[dayIndex])
     }
   })
 }
