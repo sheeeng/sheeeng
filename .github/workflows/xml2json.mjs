9e068260f8138783d6fe6402a1a2850456b7adb9
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-08-26 19:44:17 +0200

    feature: json xml converter

--- .github/workflows/xml2json.mjs
+++ .github/workflows/xml2json.mjs
@@ -1,29 +1,29 @@
 /*	This work is licensed under Creative Commons GNU LGPL License.
 
-	License: http://creativecommons.org/licenses/LGPL/2.1/
+   License: http://creativecommons.org/licenses/LGPL/2.1/
    Version: 0.9
-	Author:  Stefan Goessner/2006
-	Web:     http://goessner.net/ 
+   Author:  Stefan Goessner/2006
+   Web:     http://goessner.net/
 */
-function xml2json(xml, tab) {
+export function xml2json(xml, tab) {
    var X = {
-      toObj: function(xml) {
+      toObj: function (xml) {
          var o = {};
-         if (xml.nodeType==1) {   // element node ..
+         if (xml.nodeType == 1) {   // element node ..
             if (xml.attributes.length)   // element with attributes  ..
-               for (var i=0; i<xml.attributes.length; i++)
-                  o["@"+xml.attributes[i].nodeName] = (xml.attributes[i].nodeValue||"").toString();
+               for (var i = 0; i < xml.attributes.length; i++)
+                  o["@" + xml.attributes[i].nodeName] = (xml.attributes[i].nodeValue || "").toString();
             if (xml.firstChild) { // element has child nodes ..
-               var textChild=0, cdataChild=0, hasElementChild=false;
-               for (var n=xml.firstChild; n; n=n.nextSibling) {
-                  if (n.nodeType==1) hasElementChild = true;
-                  else if (n.nodeType==3 && n.nodeValue.match(/[^ \f\n\r\t\v]/)) textChild++; // non-whitespace text
-                  else if (n.nodeType==4) cdataChild++; // cdata section node
+               var textChild = 0, cdataChild = 0, hasElementChild = false;
+               for (var n = xml.firstChild; n; n = n.nextSibling) {
+                  if (n.nodeType == 1) hasElementChild = true;
+                  else if (n.nodeType == 3 && n.nodeValue.match(/[^ \f\n\r\t\v]/)) textChild++; // non-whitespace text
+                  else if (n.nodeType == 4) cdataChild++; // cdata section node
                }
                if (hasElementChild) {
                   if (textChild < 2 && cdataChild < 2) { // structured element with evtl. a single text or/and cdata node ..
                      X.removeWhite(xml);
-                     for (var n=xml.firstChild; n; n=n.nextSibling) {
+                     for (var n = xml.firstChild; n; n = n.nextSibling) {
                         if (n.nodeType == 3)  // text node
                            o["#text"] = X.escape(n.nodeValue);
                         else if (n.nodeType == 4)  // cdata node
@@ -55,56 +55,56 @@ function xml2json(xml, tab) {
                   if (cdataChild > 1)
                      o = X.escape(X.innerXml(xml));
                   else
-                     for (var n=xml.firstChild; n; n=n.nextSibling)
+                     for (var n = xml.firstChild; n; n = n.nextSibling)
                         o["#cdata"] = X.escape(n.nodeValue);
                }
             }
             if (!xml.attributes.length && !xml.firstChild) o = null;
          }
-         else if (xml.nodeType==9) { // document.node
+         else if (xml.nodeType == 9) { // document.node
             o = X.toObj(xml.documentElement);
          }
          else
             alert("unhandled node type: " + xml.nodeType);
          return o;
       },
-      toJson: function(o, name, ind) {
-         var json = name ? ("\""+name+"\"") : "";
+      toJson: function (o, name, ind) {
+         var json = name ? ("\"" + name + "\"") : "";
          if (o instanceof Array) {
-            for (var i=0,n=o.length; i<n; i++)
-               o[i] = X.toJson(o[i], "", ind+"\t");
-            json += (name?":[":"[") + (o.length > 1 ? ("\n"+ind+"\t"+o.join(",\n"+ind+"\t")+"\n"+ind) : o.join("")) + "]";
+            for (var i = 0, n = o.length; i < n; i++)
+               o[i] = X.toJson(o[i], "", ind + "\t");
+            json += (name ? ":[" : "[") + (o.length > 1 ? ("\n" + ind + "\t" + o.join(",\n" + ind + "\t") + "\n" + ind) : o.join("")) + "]";
          }
          else if (o == null)
-            json += (name&&":") + "null";
-         else if (typeof(o) == "object") {
+            json += (name && ":") + "null";
+         else if (typeof (o) == "object") {
             var arr = [];
             for (var m in o)
-               arr[arr.length] = X.toJson(o[m], m, ind+"\t");
-            json += (name?":{":"{") + (arr.length > 1 ? ("\n"+ind+"\t"+arr.join(",\n"+ind+"\t")+"\n"+ind) : arr.join("")) + "}";
+               arr[arr.length] = X.toJson(o[m], m, ind + "\t");
+            json += (name ? ":{" : "{") + (arr.length > 1 ? ("\n" + ind + "\t" + arr.join(",\n" + ind + "\t") + "\n" + ind) : arr.join("")) + "}";
          }
-         else if (typeof(o) == "string")
-            json += (name&&":") + "\"" + o.toString() + "\"";
+         else if (typeof (o) == "string")
+            json += (name && ":") + "\"" + o.toString() + "\"";
          else
-            json += (name&&":") + o.toString();
+            json += (name && ":") + o.toString();
          return json;
       },
-      innerXml: function(node) {
+      innerXml: function (node) {
          var s = ""
          if ("innerHTML" in node)
             s = node.innerHTML;
          else {
-            var asXml = function(n) {
+            var asXml = function (n) {
                var s = "";
                if (n.nodeType == 1) {
                   s += "<" + n.nodeName;
-                  for (var i=0; i<n.attributes.length;i++)
-                     s += " " + n.attributes[i].nodeName + "=\"" + (n.attributes[i].nodeValue||"").toString() + "\"";
+                  for (var i = 0; i < n.attributes.length; i++)
+                     s += " " + n.attributes[i].nodeName + "=\"" + (n.attributes[i].nodeValue || "").toString() + "\"";
                   if (n.firstChild) {
                      s += ">";
-                     for (var c=n.firstChild; c; c=c.nextSibling)
+                     for (var c = n.firstChild; c; c = c.nextSibling)
                         s += asXml(c);
-                     s += "</"+n.nodeName+">";
+                     s += "</" + n.nodeName + ">";
                   }
                   else
                      s += "/>";
@@ -115,20 +115,20 @@ function xml2json(xml, tab) {
                   s += "<![CDATA[" + n.nodeValue + "]]>";
                return s;
             };
-            for (var c=node.firstChild; c; c=c.nextSibling)
+            for (var c = node.firstChild; c; c = c.nextSibling)
                s += asXml(c);
          }
          return s;
       },
-      escape: function(txt) {
+      escape: function (txt) {
          return txt.replace(/[\\]/g, "\\\\")
-                   .replace(/[\"]/g, '\\"')
-                   .replace(/[\n]/g, '\\n')
-                   .replace(/[\r]/g, '\\r');
+            .replace(/[\"]/g, '\\"')
+            .replace(/[\n]/g, '\\n')
+            .replace(/[\r]/g, '\\r');
       },
-      removeWhite: function(e) {
+      removeWhite: function (e) {
          e.normalize();
-         for (var n = e.firstChild; n; ) {
+         for (var n = e.firstChild; n;) {
             if (n.nodeType == 3) {  // text node
                if (!n.nodeValue.match(/[^ \f\n\r\t\v]/)) { // pure whitespace text node
                   var nxt = n.nextSibling;
