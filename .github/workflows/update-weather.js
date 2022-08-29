d56e44869d40fc410ab79579d60a01e6d7939b74
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2022-08-29 11:01:55 +0200

    refactor: open weather & mustache

--- .github/workflows/update-weather.js
+++ .github/workflows/update-weather.js
@@ -32,6 +32,18 @@ async function setWeatherInformation() {
       DATA.city_pressure = Math.round(r.main.pressure);
       DATA.city_weather = r.weather[0].description;
       DATA.city_weather_icon = r.weather[0].icon;
+      DATA.meteorology_data_timestamp = new Date(r.dt * 1000).toLocaleString('en-GB', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        fractionalSecondDigits: '3',
+        timeZone: 'Europe/Oslo',
+        timeZoneName: 'short',
+      });
       DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
         hour: '2-digit',
         minute: '2-digit',
